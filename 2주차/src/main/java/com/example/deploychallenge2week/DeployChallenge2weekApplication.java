7b3bca69f179e33caa47bb209a5e6f5c4f61a7aa
minbo2002 <minbo2002@naver.com>
2023-10-21 17:19:17 +0900

    fisrt commit!

---
+++ 2주차/src/main/java/com/example/deploychallenge2week/DeployChallenge2weekApplication.java
@@ -0,0 +1,13 @@
+package com.example.deploychallenge2week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployChallenge2weekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeployChallenge2weekApplication.class, args);
+    }
+
+}
