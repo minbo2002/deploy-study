b359543bdc2f66ccd4545630a147de0548348e39
minbo2002 <minbo2002@naver.com>
2023-11-04 23:25:30 +0900

    chore: 폴더삭제 및 파일 이동

--- 1week/src/main/java/com/example/deploychallenge1week/domain/coupon/entity/CouponStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.deploychallenge1week.domain.coupon.entity;
-
-public enum CouponStatus {
-    PRIVATE, PUBLIC
-}
--- 1week/src/main/java/com/example/deploychallenge1week/domain/coupon/repository/CouponRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.deploychallenge1week.domain.coupon.repository;
-
-import com.example.deploychallenge1week.domain.coupon.entity.Coupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CouponRepository extends JpaRepository<Coupon, Long> {
-}
