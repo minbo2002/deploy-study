98a5150e79d164669ea5da90a4e4b21839e42bff
minbo2002 <minbo2002@naver.com>
2023-11-04 23:28:25 +0900

    feat: Jenkins SSH FreeStyle 프로젝트 배포를 위한 설정

---
+++ src/test/java/com/example/deploychallenge2week/DeployChallenge2weekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deploychallenge2week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployChallenge2weekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
